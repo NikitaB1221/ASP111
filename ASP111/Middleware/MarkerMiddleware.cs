b7e31b974a95b976f0d2741253bf471239a80a50
Bodu_wk69 <Bodu_wk69@student.itstep.org>
2023-08-18 21:13:29 +0300

    MiddlewareUpdate

--- ASP111/Middleware/MarkerMiddleware.cs
+++ ASP111/Middleware/MarkerMiddleware.cs
@@ -6,6 +6,9 @@ namespace ASP111.Middleware
     {
         private readonly RequestDelegate _next;
         private static int _cnt;
+        private static uint getRs = 0;
+        private static uint postRs = 0;
+
 
         public MarkerMiddleware(RequestDelegate next)
         {
@@ -16,6 +19,16 @@ namespace ASP111.Middleware
         public async Task InvokeAsync(HttpContext context, DataContext dataContext)
         {
             context.Items.Add("marker", $"Users: {dataContext.Users.Count()}, requests: { ++ _cnt}");
+            if (context.Request.Method.Equals("GET", StringComparison.OrdinalIgnoreCase))
+            {
+                ++getRs;
+            }
+            else if (context.Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase))
+            {
+                ++postRs;
+            }
+            context.Items["GET_Req-s"] = getRs;
+            context.Items["POST_Req-s"] = postRs;
             await _next(context);
         }
     }
